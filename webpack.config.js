f6058e7b7c15ba5cfe171358f386f56ef8b37268
Ian Sinnott <ian@iansinnott.com>
2016-08-03 14:06:21 -0700

    Add babel support

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -6,7 +8,21 @@ module.exports = {
     path: 'dist',
     filename: 'app.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css'),
+      },
+    ],
+  },
   plugins: [
+    new ExtractTextPlugin('app.css', { allChunks: true }),
     new HtmlPlugin({
       template: './src/index.html',
     }),
