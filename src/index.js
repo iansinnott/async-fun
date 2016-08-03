792b17cb4af4ba49f1f6587d7c171f447ccfdfdc
Ian Sinnott <ian@iansinnott.com>
2016-08-03 14:15:06 -0700

    Fix issue with our element creation component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import 'normalize.css';
 
-import {
-  el,
-} from './utils.js';
+import { el } from './utils.js';
+
+window.el = el;
 
 const root = document.querySelector('#root');
 
 root.appendChild(
-  el('div', { class: 'superDiv' }, 'Some Text Content')
+  el('div', { class: 'superDiv' }, [
+    "I've also heard that creating your own react clone is ",
+    el('span', null, 'SUPER FUN'),
+  ])
 );
-
-console.log('Welcome to the bundle!');
--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,3 @@
-/**
- * A simple forEach function that can be used on objects as well as arrays.
- * NOTE: No context setting is implemented so this API is not exactly the same
- * as the standard JS [].forEach.
- */
-export const forEach = (iterable, fn) => {
-  if (typeof iterable !== 'object') {
-    throw new Error(`forEach called on non iterable value "${iterable}"`);
-  }
-
-  if (Array.isArray(iterable)) {
-    return iterable.forEach(fn);
-  }
-
-  Object.keys(iterable).forEach((k) => {
-    fn(iterable[k], k);
-  });
-};
-
 const normalizeAttributeKey = (key) => {
   switch (key) {
   case 'className':
@@ -37,7 +18,7 @@ const normalizeChildren = (children) => {
     .filter(x => !!x) // Only truthy children
     .map(x => {
       if (typeof x === 'string') {
-        return document.createTextNode(children);
+        return document.createTextNode(x);
       } else if (Array.isArray(x)) {
         return normalizeChildren(x);
       } else {
@@ -46,13 +27,34 @@ const normalizeChildren = (children) => {
     });
 };
 
+/**
+ * A simple forEach function that can be used on objects as well as arrays.
+ * NOTE: No context setting is implemented so this API is not exactly the same
+ * as the standard JS [].forEach.
+ */
+export const forEach = (iterable, fn) => {
+  if (typeof iterable !== 'object') {
+    throw new Error(`forEach called on non iterable value "${iterable}"`);
+  }
+
+  if (Array.isArray(iterable)) {
+    return iterable.forEach(fn);
+  }
+
+  Object.keys(iterable).forEach((k) => {
+    fn(iterable[k], k);
+  });
+};
+
 export const el = (tagName, props, children) => {
   const element = document.createElement(tagName);
 
-  forEach(props, (v, _k) => {
-    const k = normalizeAttributeKey(_k);
-    element.setAttribute(k, v);
-  });
+  if (props) {
+    forEach(props, (v, _k) => {
+      const k = normalizeAttributeKey(_k);
+      element.setAttribute(k, v);
+    });
+  }
 
   normalizeChildren(children).forEach(x => {
     element.appendChild(x);
