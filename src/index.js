b9261af9808072369fd16f589822a093b06b9627
Ian Sinnott <ian@iansinnott.com>
2016-08-03 14:06:33 -0700

    Create utils file to hold utils

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import 'normalize.css';
+
+import {
+  el,
+} from './utils.js';
+
+const root = document.querySelector('#root');
+
+root.appendChild(
+  el('div', { class: 'superDiv' }, 'Some Text Content')
+);
+
 console.log('Welcome to the bundle!');
---
+++ src/utils.js
@@ -0,0 +1,63 @@
+/**
+ * A simple forEach function that can be used on objects as well as arrays.
+ * NOTE: No context setting is implemented so this API is not exactly the same
+ * as the standard JS [].forEach.
+ */
+export const forEach = (iterable, fn) => {
+  if (typeof iterable !== 'object') {
+    throw new Error(`forEach called on non iterable value "${iterable}"`);
+  }
+
+  if (Array.isArray(iterable)) {
+    return iterable.forEach(fn);
+  }
+
+  Object.keys(iterable).forEach((k) => {
+    fn(iterable[k], k);
+  });
+};
+
+const normalizeAttributeKey = (key) => {
+  switch (key) {
+  case 'className':
+    return 'class';
+  case 'htmlFor':
+    return 'for';
+  default:
+    return key;
+  }
+};
+
+/**
+ * This is not very sophisticated and is likely error prone.
+ */
+const normalizeChildren = (children) => {
+  if (!Array.isArray(children)) return normalizeChildren([children]);
+  return children
+    .filter(x => !!x) // Only truthy children
+    .map(x => {
+      if (typeof x === 'string') {
+        return document.createTextNode(children);
+      } else if (Array.isArray(x)) {
+        return normalizeChildren(x);
+      } else {
+        return x;
+      }
+    });
+};
+
+export const el = (tagName, props, children) => {
+  const element = document.createElement(tagName);
+
+  forEach(props, (v, _k) => {
+    const k = normalizeAttributeKey(_k);
+    element.setAttribute(k, v);
+  });
+
+  normalizeChildren(children).forEach(x => {
+    element.appendChild(x);
+  });
+
+  return element;
+};
+
