56d570580d41331d9ddd1cc2fb95b0d3e380624b
Ian Sinnott <ian@iansinnott.com>
2016-08-03 15:25:33 -0700

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import 'normalize.css';
 
+import { forEach } from './utils.js';
 import { div, span } from './dom.js';
 
 const root = document.querySelector('#root');
 
-root.appendChild(
+const render = (dom, node) => {
+  forEach(node.children, x => node.removeChild(x));
+  root.appendChild(dom);
+};
+
+const App = () => (
   div({ className: 'superDiv' }, [
     "I've also heard that creating your own react clone is ",
     span(null, 'SUPER FUN'),
+    div({
+      style: { color: 'orange' },
+    }, Math.random()),
   ])
 );
+
+// Initial render
+render(App(), root);
+
+// Rerender to display new randomized values occasionally
+setInterval(() => {
+  render(App(), root);
+}, 1000);
--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,3 @@
-const normalizeAttributeKey = (key) => {
-  switch (key) {
-  case 'className':
-    return 'class';
-  case 'htmlFor':
-    return 'for';
-  default:
-    return key;
-  }
-};
-
-/**
- * This is not very sophisticated and is likely error prone.
- */
-const normalizeChildren = (children) => {
-  if (!Array.isArray(children)) return normalizeChildren([children]);
-  return children
-    .filter(x => !!x) // Only truthy children
-    .map(x => {
-      if (typeof x === 'string') {
-        return document.createTextNode(x);
-      } else if (Array.isArray(x)) {
-        return normalizeChildren(x);
-      } else {
-        return x;
-      }
-    });
-};
-
 /**
  * A simple forEach function that can be used on objects as well as arrays.
  * NOTE: No context setting is implemented so this API is not exactly the same
@@ -46,20 +17,8 @@ export const forEach = (iterable, fn) => {
   });
 };
 
-export const el = (tagName, props, children) => {
-  const element = document.createElement(tagName);
-
-  if (props) {
-    forEach(props, (v, _k) => {
-      const k = normalizeAttributeKey(_k);
-      element.setAttribute(k, v);
-    });
-  }
-
-  normalizeChildren(children).forEach(x => {
-    element.appendChild(x);
-  });
-
-  return element;
-};
-
+/**
+ * A super simple partial function. It only does partial application with one
+ * argument.
+ */
+export const partial = (fn, arg) => (...args) => fn(arg, ...args);
